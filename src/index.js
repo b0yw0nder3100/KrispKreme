0979453828d595129120ac5c3a3e6f01020b7a1d
Martins Ikpe <martinsikpe25@gmail.com>
2022-03-18 23:49:59 +0100

    Initial commit
    Finished the design of the landing page for laptop screen sizes only

--- src/index.js
+++ src/index.js
@@ -1,17 +1,110 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
+import ReactDom from "react-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import logo from './Assets/logo.png'
+import one from './Assets/1.png'
+import two from './Assets/2.png'
+import three from './Assets/3.png'
+import four from './Assets/4.png'
+import pack1 from './Assets/pack1.png'
+import add from './Assets/add.png'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Index = () => {
+    return (
+        <>
+            <Header></Header>
+            <Main></Main>
+        </>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <div className="nav-section-one">
+                    <img src={logo} alt="lgog"></img>
+                </div>
+
+                <div className="nav-section-two">
+                    <ul>
+                        <li>Home</li>
+                        <li>Menu</li>
+                        <li>Delivery</li>
+                    </ul>
+                </div>
+
+                <div className="nav-section-three">
+                    <i class="fas fa-phone-alt"></i>
+                    <i class="fas fa-shopping-cart"></i>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+const Main = () => {
+    return (
+        <main>
+            <section className="hero-section">
+
+                <section className="hero-images">
+                    <img src={one} alt="one" className="one"></img>
+                    <img src={two} alt="two" className="twos"></img>
+                    <img src={three} alt="three" className="three"></img>
+                    <img src={four} alt="four" className="four"></img>
+                    <div className="three-twos">
+                        <img src={three} alt="four" className="three-two"></img>
+                    </div>
+                </section>
+
+                <section className="hero-section-one">
+                    <div className="hero-section-text">
+                        <h1>Hey! <br />
+                            do you like <br />
+                            my six <br />
+                            pac?
+                        </h1>
+                    </div>
+                    <div>
+                        <button>Menu</button>
+                    </div>
+                </section>
+
+                <section className="hero-section-two">
+
+                    <div className="two">
+                        <div className="food-packs">
+                            <img src={pack1} alt="pack1" className="food" />
+                            <img src={add} alt="pack1" className="add" />
+                        </div>
+                    </div>
+
+                    <div className="order-menus">
+                        <div className="order-menu locations">
+                            <p>Location</p>
+                            <select>
+                                <option>Lekki</option>
+                                <option>Lekki</option>
+                            </select>
+                        </div>
+                        <div className="order-menu quantity">
+                            <p>QTY</p>
+                            <input type='number'></input>
+                        </div>
+                        <div className="order-menu total">
+                            <p>Total</p>
+                            <h6>#3,400</h6>
+                        </div>
+                        <div className="order-menu order">
+                            <button>Order Now</button>
+                        </div>
+                    </div>
+                </section>
+            </section>
+        </main>
+    )
+}
+
+ReactDom.render(<Index />, document.getElementById('root'));
